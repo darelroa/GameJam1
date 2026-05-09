fd9c8c62ea7dd606dc000dec3015eb20ddc4ad4c
Doris Wei <doris.xy.wei@gmail.com>
2026-05-09 13:02:55 +1000

    Added movement features
    Added kill zone respawn
    Added camera tracking movement
    Adding new platforms to jump onto

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scences/world/test_scene.tscn")
+	get_tree().change_scene_to_file("res://scences/world/level_1.tscn")
 
 
 func _on_quit_button_pressed() -> void:
